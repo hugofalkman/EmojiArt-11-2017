f6567ecdd3e3c80bfaa31c96935c40f4347bce90
hugofalkman <hugo@falkman.net>
2018-02-06 00:24:19 +0100

    Lecture 12 complete. Second demo collection view and drag/drop

---
+++ EmojiArt-11-2017/EmojiCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  EmojiCollectionViewCell.swift
+//  EmojiArt-11-2017
+//
+//  Created by H Hugo Falkman on 2018-02-05.
+//  Copyright © 2018 H Hugo Falkman. All rights reserved.
+//
+
+import UIKit
+
+class EmojiCollectionViewCell: UICollectionViewCell {
+ 
+    @IBOutlet weak var label: UILabel!
+}
